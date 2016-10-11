1f3ffc3862b4387bbf35e5f08a5f1b32c586c3c2
Bruce Li <ascendbruce@gmail.com>
2016-10-11 19:35:53 +0800

    Add setup-zsh.sh script

---
+++ setup-zsh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Prerequisites:
+#
+# * Able to use git command
+# * Done uploading public key to GitHub ascendbruce account
+# * Double check the oh-my-zsh install command
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Seinh/git-prune.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-prune
+
+mkdir -p ~/Projects
+git clone git@github.com:ascendbruce/dotfiles.git ~/Projects/dotfiles
+
+ln -s -f ~/Projects/dotfiles/.zshrc .zshrc
+ln -s -f ~/Projects/dotfiles/.gemrc .gemrc
+
+source ~/.zshrc
