bc9c0d1463a5d0de8c05d0510fbafd72c8383bac
Bruce Li <ascendbruce@gmail.com>
2016-10-17 10:38:08 +0800

    Refine setup-get script

--- setup-git.sh
+++ setup-git.sh
@@ -4,6 +4,10 @@
 #
 # * Able to use git command
 
+brew install git
+
+ln -sf ~/Projects/dotfiles/.gitignore_global ~/.gitignore_global
+
 git config --global core.commentchar ";"
 git config --global core.editor 'vim'
 git config --global core.excludesfile ~/.gitignore_global
