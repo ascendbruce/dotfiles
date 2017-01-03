2cf1188c6aed6c23ae1abbd5a434996b177b488a
Bruce Li <ascendbruce@gmail.com>
2017-01-03 10:29:50 +0800

    Add .tigrc and placeholder for .pgpass

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -12,5 +12,7 @@ ln -sFi ~/Projects/dotfiles/.gemrc ~/.gemrc
 ln -sFi ~/Projects/dotfiles/.aliases ~/.aliases
 ln -sFi ~/Projects/dotfiles/.custom_functions ~/.custom_functions
 ln -sFi ~/Projects/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sFi ~/Projects/dotfiles/.tigrc ~/.tigrc
+touch ~/.pgpass
 
 source ~/.zshrc
